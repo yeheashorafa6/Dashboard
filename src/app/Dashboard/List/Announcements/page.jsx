51e869aed1e72c8e6ff4adcbdbe6a41f95c86746
Yehea Shorafa <yeheashorafa6@gmail.com>
2025-01-13 17:46:39 +0200

    edit announcement page

--- src/app/Dashboard/List/Announcements/page.jsx
+++ src/app/Dashboard/List/Announcements/page.jsx
@@ -53,74 +53,77 @@ const renderRow = (item) => (
   </tr>
 );
 async function AnnouncementsListPage({ searchParams }) {
-  const { page, ...queryParams } = searchParams;
-  const p = page ? parseInt(page) : 1;
-  const query = {};
+  // const { page, ...queryParams } = searchParams;
+  // const p = page ? parseInt(page) : 1;
+  // const query = {};
 
-  if (queryParams) {
-    for (const [key, value] of Object.entries(queryParams)) {
-      if (value !== undefined) {
-        switch (key) {
-          case "search":
-            const lowerCaseValue = value.toLowerCase();
-            query.title = { contains: lowerCaseValue };
-            break;
-          default:
-            break;
-        }
-      }
-    }
-  }
-  const [data, count] = await prisma.$transaction([
-    prisma.announcement.findMany({
-      where: query,
-      include: {
-        class: true,
-      },
-      take: ITEM_PER_PAGE,
-      skip: (p - 1) * ITEM_PER_PAGE,
-    }),
-    prisma.announcement.count({
-      where: query,
-    }),
-  ]);
+  // if (queryParams) {
+  //   for (const [key, value] of Object.entries(queryParams)) {
+  //     if (value !== undefined) {
+  //       switch (key) {
+  //         case "search":
+  //           const lowerCaseValue = value.toLowerCase();
+  //           query.title = { contains: lowerCaseValue };
+  //           break;
+  //         default:
+  //           break;
+  //       }
+  //     }
+  //   }
+  // }
+  // const [data, count] = await prisma.$transaction([
+  //   prisma.announcement.findMany({
+  //     where: query,
+  //     include: {
+  //       class: true,
+  //     },
+  //     take: ITEM_PER_PAGE,
+  //     skip: (p - 1) * ITEM_PER_PAGE,
+  //   }),
+  //   prisma.announcement.count({
+  //     where: query,
+  //   }),
+  // ]);
   // console.log(data);
   return (
-    <div className="bg-white rounded-md p-4 m-4 mt-2 ">
-      {/* TOP */}
-      <div className="flex justify-between items-center">
-        <h1 className="hidden md:block  text-lg font-semibold">
-          All Announcements
-        </h1>
-        <div className="flex flex-col md:flex-row items-center justify-center gap-3 w-full md:w-auto">
-          {/* SEARCH BAR */}
-          <TableSearch />
-          {/* ICON BAR */}
-          <div className="flex items-center gap-3 justify-end">
-            <button className=" rounded-full w-8 h-8 flex justify-center items-center cursor-pointer bg-secondaryYellow p-2 text-black">
-              <GiSettingsKnobs size={17} />
-            </button>
-            <button className=" rounded-full flex justify-center items-center cursor-pointer bg-secondaryYellow p-2 text-black">
-              <BsSortDown size={17} />
-            </button>
-            {role === "admin" && (
-              <FormModal table={"announcement"} type={"create"}  />
-            )}
-          </div>
-        </div>
-      </div>
-      {/* LIST */}
-      <div>
-        <Table
-          columns={columns}
-          renderRow={renderRow}
-          data={data}
-        />
-      </div>
-      {/* PAGINATION */}
-      <div>
-        <Pagination page={p} count={count}/>
-      </div>
+    // <div className="bg-white rounded-md p-4 m-4 mt-2 ">
+    //   {/* TOP */}
+    //   <div className="flex justify-between items-center">
+    //     <h1 className="hidden md:block  text-lg font-semibold">
+    //       All Announcements
+    //     </h1>
+    //     <div className="flex flex-col md:flex-row items-center justify-center gap-3 w-full md:w-auto">
+    //       {/* SEARCH BAR */}
+    //       <TableSearch />
+    //       {/* ICON BAR */}
+    //       <div className="flex items-center gap-3 justify-end">
+    //         <button className=" rounded-full w-8 h-8 flex justify-center items-center cursor-pointer bg-secondaryYellow p-2 text-black">
+    //           <GiSettingsKnobs size={17} />
+    //         </button>
+    //         <button className=" rounded-full flex justify-center items-center cursor-pointer bg-secondaryYellow p-2 text-black">
+    //           <BsSortDown size={17} />
+    //         </button>
+    //         {role === "admin" && (
+    //           <FormModal table={"announcement"} type={"create"}  />
+    //         )}
+    //       </div>
+    //     </div>
+    //   </div>
+    //   {/* LIST */}
+    //   <div>
+    //     <Table
+    //       columns={columns}
+    //       renderRow={renderRow}
+    //       data={data}
+    //     />
+    //   </div>
+    //   {/* PAGINATION */}
+    //   <div>
+    //     <Pagination page={p} count={count}/>
+    //   </div>
+    // </div>
+    <div>
+      announcments
     </div>
   );
 }
